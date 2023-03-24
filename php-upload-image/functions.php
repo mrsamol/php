8aef990b7986263ee7704e9d01df80709c687dac
samol <samol.it.com@gmail.com>
2023-03-24 13:43:07 +0700

    PHP upload image file

---
+++ php-upload-image/functions.php
@@ -0,0 +1,7 @@
+<?php
+class UplaodImage
+{
+    public function uplaodImageFile($path){
+        return $path;
+    }
+}
---
+++ php-upload-image/index.php
@@ -0,0 +1,5 @@
+<?php
+    include_once('functions.php');
+
+    $uplaod = new UplaodImage();
+    echo $uplaod->uplaodImageFile('images');
