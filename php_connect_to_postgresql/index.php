a0e5ca7ec75ff8bc2eb8586e549d4feecc79b397
samol <samol.it.com@gmail.com>
2023-03-30 09:20:45 +0700

    get data from database

--- php_connect_to_postgresql/index.php
+++ php_connect_to_postgresql/index.php
@@ -5,5 +5,12 @@ include_once('connect.php');
 $pdo = connectToPostgreSQL('127.0.0.1', '5432', 'mylearn', 'postgres', '123');
 
 $data = getDataFromPostgreSQL($pdo, 'tbl_users');
-
-var_dump($data);
+$result = [];
+if (!empty($data)) {
+    foreach($data as $value){
+        $result[] = $value;
+    }
+    echo $result[0]['email'];
+} else {
+    echo "No data found";
+}
