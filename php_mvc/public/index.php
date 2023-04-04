685d947181cae2f84171df537fe6b6da3b4aaea2
samol <samol.it.com@gmail.com>
2023-04-04 13:52:23 +0700

    PHP MVC

---
+++ php_mvc/public/index.php
@@ -0,0 +1,7 @@
+<?php
+    define('ROOT',dirname(__DIR__,1));
+    define('PUBLIC_PATH',ROOT . '/public');
+    define('SRC_PATH',ROOT . '/src');
+    define('VIEW_PATH',SRC_PATH . '/views');
+    define('CONTROLLER_PATH',SRC_PATH . '/controllers');
+    define('MODEL_PATH',SRC_PATH . '/models');
